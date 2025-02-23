791677c160f2d515b9432e0465e4a8112636bbcc
Yuzefovych-Alex <yuzefovych.alex@gmail.com>
2025-02-23 13:01:06 +0100

    test-1

---
+++ Assets/Scripts/System/FireDamageSystem.cs
@@ -0,0 +1,17 @@
+using Unity.Burst;
+using Unity.Entities;
+
+[BurstCompile]
+public partial struct FireDamageSystem : ISystem
+{
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach (var (health, fire) in SystemAPI.Query<RefRW<Health>, RefRO<FireLevel>>().WithAll<HouseTag>())
+        {
+            if (fire.ValueRO.Value > 0)
+            {
+                health.ValueRW.Value -= fire.ValueRO.Value; 
+            }
+        }
+    }
+}
